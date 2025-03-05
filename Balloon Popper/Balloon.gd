d661226af2c3a6e8b8417d4d754962864c437e51
beltranbot <beltranbot@gmail.com>
2025-03-04 22:38:11 -0500

    6. Baloon Popper - Part 3

---
+++ Balloon Popper/Balloon.gd
@@ -0,0 +1,26 @@
+extends Area3D
+
+var clicks_to_pop: int = 3
+var size_increase: float = 0.2
+var score_to_give: int = 1
+
+var _event: InputEvent
+
+
+func _on_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	_event = event
+	if _was_left_mouse_button_pressed():
+		scale += Vector3.ONE * size_increase
+		clicks_to_pop -= 1
+		if clicks_to_pop == 0:
+			# access the BalloonManager node and increase the score
+			get_node("/root/Main").increase_score(score_to_give)
+			queue_free()
+
+### private ###
+
+# detects when the left mouse button is pressed
+func _was_left_mouse_button_pressed():
+	return _event is InputEventMouseButton and \
+		_event.button_index == MOUSE_BUTTON_LEFT and \
+		_event.pressed
